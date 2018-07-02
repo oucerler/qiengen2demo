09755e0f1552cb71be91701ff5299ee65619a81e
oucerler <oucerler@gmail.com>
2018-07-02 11:36:11 +0200

    first commit

---
+++ Qien.java
@@ -0,0 +1,20 @@
+class Qien{
+	public static void main(String[] args) {
+		System.out.println("de tekst die je wilt typen");
+		
+		int leeftijd = 19;
+		
+
+		if(leeftijd < 16){
+			System.out.println("geen alcohol");
+		}
+		if(leeftijd >= 16 && leeftijd < 18){
+			System.out.println("geen bier en wijn");
+		}
+		if(leeftijd > 18){
+			System.out.println("vrij");
+		}
+
+	}
+
+}
